bb7d7baa78be5eb6a2c2a39390b7ee96603dd82a
Hirusha Ranaweera <leavingnadir.inc@gmail.com>
2025-10-02 20:07:37 +0530

    Hero section commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-const App = () => {
+import Hero from "./components/Hero";
+import About from "./components/About";
+
+function App() {
   return (
-    <main>
-      <h1 className='text-5xl font-bold text-violet-300 font-robert-medium'>Welcom to awrdds</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+      <About />
+      <section className="z-0 min-h-screen bg-blue-500"></section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
